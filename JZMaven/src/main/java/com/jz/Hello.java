ae54c5b65f855fc70b722d95f9ba3dc34df39643
JZFeng <matthew.fjz@gmail.com>
2018-11-08 07:44:05 -0800

    x

--- JZMaven/src/main/java/com/jz/Hello.java
+++ JZMaven/src/main/java/com/jz/Hello.java
@@ -4,7 +4,11 @@ import java.io.*;
 
 public class Hello {
     public static void main(String[] args) {
-        int a = 0x111;
-        System.out.println(a);
+        final int a = 0b111;
+        final double TAX_RATE = 0.2d;
+        System.out.println(TAX_RATE);
+        long l = 9000_000_000_000L;
+        System.out.println(Long.toHexString(l));
+        System.out.println(Long.toBinaryString(l));
     }
 }
