2f9b359d6e7feff9e83f7caf0891bc72d857e143
JZFeng <matthew.fjz@gmail.com>
2018-11-17 16:10:22 -0800

    Old Class :Date, Calendar New Class: LocalDate, LocalTime, LocalDateTime, ZonedDateTime, Instant , DateTimeFormator

---
+++ JZMaven/src/main/java/com/jz/datetime/DateDemo.java
@@ -0,0 +1,34 @@
+package com.jz.datetime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateDemo {
+    public static void main(String[] args) throws ParseException {
+        System.out.println(System.currentTimeMillis());
+
+        Date now = new Date();
+        System.out.println(now);
+        long epotchTime = now.getTime();
+        System.out.println(epotchTime);
+        now = new Date(epotchTime);
+        System.out.println(now);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(format.format(now));
+
+        String s1 = "2018-11-15 14:57:50";
+        now = format.parse(s1);
+        System.out.println(now);
+
+        String s2 = "Nov/15/2018 15:03:25";
+        now = new SimpleDateFormat("MMM/dd/yyyy HH:mm:ss", Locale.US).parse(s2);
+        System.out.println(now);
+
+
+
+    }
+}
---
+++ JZMaven/src/main/java/com/jz/datetime/LocalDateTimeCalculate.java
@@ -0,0 +1,30 @@
+package com.jz.datetime;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.TemporalAdjusters;
+
+public class LocalDateTimeCalculate {
+    public static void main(String[] args) {
+        LocalDate firstDay = LocalDate.now().withDayOfMonth(1);
+        System.out.println(firstDay);
+        LocalDate firstDay2 = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(firstDay.equals(firstDay2));
+
+        System.out.println(LocalDate.now().withMonth(1));
+        LocalDate firstSaturday = LocalDate.now().with(TemporalAdjusters.firstInMonth(DayOfWeek.SATURDAY));
+        System.out.println(LocalDate.now().isAfter(firstSaturday));
+        System.out.println("Leap Year:  " + firstSaturday.isLeapYear());
+
+
+        System.out.println( LocalDate.of(2050, 3,25).toEpochDay() - LocalDate.now().toEpochDay() );
+        Period p = LocalDate.now().until(LocalDate.of(2050,1,19));
+        System.out.println(p);
+        System.out.println(p.getYears() + " Years " + p.getMonths() + " Months " + p.getDays() + " Days" );
+
+
+    }
+}
+
