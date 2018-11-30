301958744f0e779aa43982dfa1910347ab099641
JZFeng <matthew.fjz@gmail.com>
2018-11-30 07:49:49 -0800

    Exception demo

---
+++ JZMaven/src/main/java/com/jz/Exception/BaseException.java
@@ -0,0 +1,19 @@
+package com.jz.Exception;
+
+public class BaseException extends RuntimeException {
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ JZMaven/src/main/java/com/jz/Exception/ThrowExceptionDemo.java
@@ -0,0 +1,47 @@
+package com.jz.Exception;
+
+public class ThrowExceptionDemo {
+    public static void main(String[] args) throws Exception {
+        process1("ABC");
+//        process2("ABC");
+    }
+
+    public static void process1(String str) throws Exception {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            for (Throwable t : e.getSuppressed()) {
+                t.printStackTrace();
+            }
+            throw new IllegalArgumentException(e);
+        } finally {
+            System.out.println("finally...");
+        }
+    }
+
+    public static void process2(String str) throws Exception {
+        Exception origin = null;
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            origin = e;
+            throw new IllegalArgumentException(e);
+        } finally {
+            try {
+                throw new NullPointerException();
+            } catch (NullPointerException e) {
+                if (origin != null) {
+                    origin.addSuppressed(e);
+                } else {
+                    origin = e;
+                }
+            }
+
+            if (origin != null) {
+                throw origin;
+            }
+        }
+    }
+
+}
