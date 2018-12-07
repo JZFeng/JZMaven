1e28a6d50370c07b43fdf41e29422cf8ef35fd5f
JZFeng <matthew.fjz@gmail.com>
2018-12-06 17:48:55 -0800

    updated

--- JZMaven/src/main/java/com/jz/designPattern/decorator/dress/Entry.java
+++ JZMaven/src/main/java/com/jz/designPattern/decorator/dress/Entry.java
@@ -5,7 +5,18 @@ public class Entry {
         Person person = new Person("JZ");
         IPerson p = new Jeans(new Sunglass( new Tie(person)));
         System.out.println(person.getName() + " is wearing : ");
-        System.out.println(p.getClothes().trim());
+        System.out.println(removeLastAddSign(p.getClothes()));
     }
 
+    private static String removeLastAddSign(String str) {
+        if(str == null || str.length() == 0) {
+            return "";
+        }
+
+        int index = str.lastIndexOf("+");
+        str = str.substring(0, index).trim();
+
+        return str;
+
+    }
 }
