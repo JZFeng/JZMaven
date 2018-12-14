033740a793aebb748babd5bdf2fd8ef315621bab
JZFeng <matthew.fjz@gmail.com>
2018-12-14 08:20:23 -0800

    delete annotation files

--- JZMaven/src/main/java/com/jz/annotation/Utility.java
+++
@@ -1,9 +0,0 @@
-package com.jz.annotation;
-
-@Description(value = "This is a very useful utility class.")
-public class Utility {
-    @Author(name = "JZ", group = "QE")
-    public String work() {
-        return "work over";
-    }
-}
