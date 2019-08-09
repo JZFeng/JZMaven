c6179b6efe5a534328df73ae2d030df4615b7f42
JZFeng <matthew.fjz@gmail.com>
2019-08-09 10:22:26 -0700

    Version 2: keep server always alive

--- JZMaven/src/main/java/com/jz/java/network/tcp/TCPClient.java
+++ JZMaven/src/main/java/com/jz/java/network/tcp/TCPClient.java
@@ -3,15 +3,18 @@ package com.jz.java.network.tcp;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class TCPClient {
   public static void main(String[] args) throws IOException {
+    String cmd;
+    try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+      cmd =bufferedReader.readLine();
+    }
 
-    try (Socket socket = new Socket(InetAddress.getLoopbackAddress(), 9090)){
-      try ( BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),StandardCharsets.UTF_8))) {
-        try( BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),StandardCharsets.UTF_8))) {
-          bw.write("Time\n");
+    try (Socket socket = new Socket(InetAddress.getLoopbackAddress(), 9090)) {
+      try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+          bw.write(cmd + "\n");
           bw.flush();
           String response = br.readLine();
           System.out.println(response);
--- JZMaven/src/main/java/com/jz/java/network/tcp/TCPServer.java
+++ JZMaven/src/main/java/com/jz/java/network/tcp/TCPServer.java
@@ -1,31 +1,55 @@
 package com.jz.java.network.tcp;
 
-
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 public class TCPServer {
   public static void main(String[] args) throws IOException {
     ServerSocket ss = new ServerSocket(9090);
-    System.out.println("Server is ready.");
-    Socket socket = ss.accept();
+    System.out.println("Server is ready!");
+    while (true) {
+      Socket socket = ss.accept();
+      TimeHandler timeHandler = new TimeHandler(socket);
+      new Thread(timeHandler).start();
+    }
+  }
+}
 
-      try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
-        try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
-          String request = br.readLine();
-          if(request.equalsIgnoreCase("Time")) {
-            bw.write(LocalDateTime.now().toString());
-            bw.flush();
-          } else {
-            bw.write("Invalid Request");
-            bw.flush();
-          }
-        }
-      }
+
+class TimeHandler implements Runnable {
+  private Socket socket;
+
+  TimeHandler(Socket socket) {
+    this.socket = socket;
   }
 
+  @Override
+  public void run() {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+      try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+        String cmd = br.readLine();
+        String response ;
+        if (cmd.equalsIgnoreCase("time")) {
+          response = LocalDateTime.now().toString();
+        } else if (cmd.equalsIgnoreCase("q")) {
+          response = "Bye!";
+        } else {
+          response = "Invalid request.";
+        }
+        bw.write(response);
+        bw.flush();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        socket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
 }
