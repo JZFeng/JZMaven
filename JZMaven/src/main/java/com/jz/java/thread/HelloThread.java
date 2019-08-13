84366d8c75b7ad80e69e8585a23ebfddb2deb7f8
JZFeng <matthew.fjz@gmail.com>
2019-08-13 07:55:10 -0700

    new Thread(new Runnable() {})
    class MyThread

---
+++ JZMaven/src/main/java/com/jz/java/thread/HelloThread.java
@@ -0,0 +1,51 @@
+package com.jz.java.thread;
+
+public class HelloThread {
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println("Main Start...");
+    Thread t1 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        for(int i = 0 ; i < 3; i++) {
+          System.out.println("Anonymous Class as thread");
+        }
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+    t1.start();
+
+    Thread t2 = new Thread(new MyThread("Tom"));
+    t2.start();
+
+    Thread t3 = new Thread(new MyThread("Bob"));
+    t3.start();
+
+    System.out.println("Main End...");
+  }
+
+}
+
+class MyThread implements Runnable {
+  private String name;
+
+  MyThread(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public void run() {
+    for(int i = 0 ; i < 3; i++) {
+      System.out.println("Hello" + name );
+    }
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
