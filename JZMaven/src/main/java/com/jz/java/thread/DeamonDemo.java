d6bf926ac54fb6a035c5ec3d895c4ae84e85b84d
JZFeng <matthew.fjz@gmail.com>
2019-08-16 07:47:49 -0700

    Daemon thread is used to serve other threads. Daemon thread cannot keep resource. Daemon thread will end all other threads and end JVM process.

---
+++ JZMaven/src/main/java/com/jz/java/thread/DeamonDemo.java
@@ -0,0 +1,30 @@
+package com.jz.java.thread;
+
+import java.time.LocalTime;
+
+public class DeamonDemo {
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println("Main start:");
+    DeamonThread thread = new DeamonThread();
+    thread.setDaemon(true);
+    thread.start();
+    Thread.sleep(5000);
+
+    System.out.println("Main end:");
+  }
+}
+
+class DeamonThread extends Thread {
+
+  @Override
+  public void run() {
+    while (true) {
+      System.out.println(LocalTime.now());
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
