f1e84b04ec66bd35fdd12e5a8f527c5c8dea2251
JZFeng <matthew.fjz@gmail.com>
2019-08-17 16:32:01 -0700

    ReentrantReadWriteLock读写锁，读多写少的场景。

---
+++ JZMaven/src/main/java/com/jz/java/thread/concurrent/ReadWriteLockDemo.java
@@ -0,0 +1,86 @@
+package com.jz.java.thread.concurrent;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockDemo {
+  public static final int LOOP = 1000;
+  public static void main(String[] args) throws InterruptedException {
+    Counter1 counter1 = new Counter1();
+    AddThread1 addThread1 = new AddThread1(counter1);
+    DecThread1 decThread1 = new DecThread1(counter1);
+    addThread1.start();
+    decThread1.start();
+    addThread1.join();
+    decThread1.join();
+    System.out.println(counter1.getCount());
+    System.out.println("Main End!");
+
+  }
+}
+
+class AddThread1 extends Thread {
+  private Counter1 counter1;
+
+  AddThread1(Counter1 counter1) {
+    this.counter1 = counter1;
+  }
+
+  @Override
+  public void run(){
+    for(int i = 0 ; i < ReadWriteLockDemo.LOOP; i++) {
+      counter1.add(1);
+    }
+  }
+}
+class DecThread1 extends Thread {
+  private Counter1 counter1;
+
+  DecThread1(Counter1 counter1) {
+    this.counter1 = counter1;
+  }
+
+  @Override
+  public void run(){
+    for(int i = 0 ; i < ReadWriteLockDemo.LOOP; i++) {
+      counter1.dec(1);
+    }
+  }
+}
+
+class Counter1 {
+  private int count = 0;
+
+  private ReadWriteLock lock = new ReentrantReadWriteLock();
+  private Lock rlock = lock.readLock();
+  private Lock wlock = lock.writeLock();
+
+  public int getCount() {
+    rlock.lock();
+    try {
+      return count;
+    } finally {
+      rlock.unlock();
+    }
+  }
+
+  public void add(int n) {
+    wlock.lock();
+    try{
+      count += n;
+    }finally {
+      wlock.unlock();
+    }
+  }
+
+  public void dec(int n) {
+    wlock.lock();
+    try{
+      count -= n;
+    } finally {
+      wlock.unlock();
+    }
+  }
+
+}
--- JZMaven/src/main/java/com/jz/java/thread/concurrent/ReentrantLockDemo.java
+++ JZMaven/src/main/java/com/jz/java/thread/concurrent/ReentrantLockDemo.java
@@ -1,6 +1,10 @@
 package com.jz.java.thread.concurrent;
 
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ReentrantLockDemo {
   public static final int LOOP = 1000;
 
@@ -16,6 +20,35 @@ public class ReentrantLockDemo {
     System.out.println("Main End.");
   }
 
+}
+
+class Counter {
+
+  private int count = 0;
+
+  private Lock lock = new ReentrantLock();
+
+  public int getCount() {
+    return count;
+  }
+
+  public void add(int n) throws InterruptedException {
+    lock.tryLock(1, TimeUnit.SECONDS);
+    try {
+      count += n;
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public void dec(int n) throws InterruptedException {
+    lock.tryLock(1, TimeUnit.SECONDS);
+    try {
+      count -= n;
+    } finally {
+      lock.unlock();
+    }
+  }
 }
 
 class AddThread extends Thread {
@@ -54,5 +87,6 @@ class DecThread extends Thread {
       }
     }
   }
+
 
 }
