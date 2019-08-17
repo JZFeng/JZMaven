e722cdc30fb680da827ec7da6d11a07a50ddbcc9
JZFeng <matthew.fjz@gmail.com>
2019-08-17 08:00:35 -0700

    sync demo 1.0; lock object should be final (non-changable)

--- JZMaven/src/main/java/com/jz/java/thread/sync/variable/SyncDemo.java
+++ JZMaven/src/main/java/com/jz/java/thread/sync/variable/SyncDemo.java
@@ -1,11 +1,11 @@
-package com.jz.java.thread.sync;
+package com.jz.java.thread.sync.variable;
 
 public class SyncDemo {
   public static final int LOOP = 10000;
 
   public static int count = 0;
 
-  public static Object lock = new Object();
+  public static final Object lock = new Object();
 
   public static void main(String[] args) throws InterruptedException {
     Thread t1 = new AddThread();
