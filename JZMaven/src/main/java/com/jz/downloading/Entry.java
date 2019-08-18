eafd7965082bc0cbd9d3fdf54bd41f124545f946
JZFeng <matthew.fjz@gmail.com>
2019-08-17 18:24:05 -0700

    Refactoring

--- JZMaven/src/main/java/com/jz/downloading/Entry.java
+++ JZMaven/src/main/java/com/jz/downloading/Entry.java
@@ -12,7 +12,8 @@ public class Entry {
   private static final String file = "/Users/jzfeng/Documents/download.txt";
 
   public static void main(String[] args) throws IOException, InterruptedException {
-
+    long start = System.currentTimeMillis();
+    int num_of_lines = 0;
     boolean isValid = validateURLLinks(new File("/Users/jzfeng/Documents/download.txt"));
     if (!isValid) {
       throw new RuntimeException("Duplicate URLs in the download.txt file.");
@@ -22,6 +23,7 @@ public class Entry {
     String line = br.readLine();
 
     while (line != null && line.trim().length() > 0) {
+      num_of_lines++;
       String[] strs = line.split(",");
       String filename = strs[0] + "_" + strs[1] + ".mp4";
       String url = "https://www.feiyangedu.com" + strs[2];
@@ -29,11 +31,13 @@ public class Entry {
       InputStream in = new URL(url).openStream();
       Files.copy(in, Paths.get("/Users/jzfeng/Documents/Java教程/" +
           filename), StandardCopyOption.REPLACE_EXISTING);
-      Thread.sleep(60000);
       line = br.readLine();
     }
 
+    long end = System.currentTimeMillis();
     br.close();
+    System.out.println("Downloaded " + num_of_lines + " files, " + (end - start) / 1000 + " seconds used.");
+
   }
 
 
