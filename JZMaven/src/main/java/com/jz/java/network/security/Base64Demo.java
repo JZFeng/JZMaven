3561e797d62232653986a712db9690a632da2e17
JZFeng <matthew.fjz@gmail.com>
2019-09-08 09:48:14 -0700

    Base64编码，目的是把二进制数据变成字符表示的编码。

---
+++ JZMaven/src/main/java/com/jz/java/network/security/Base64Demo.java
@@ -0,0 +1,16 @@
+package com.jz.java.network.security;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Base64Demo {
+  public static void main(String[] args) throws UnsupportedEncodingException {
+    String original = "Hello 编码测试";
+    String s = Base64.getUrlEncoder().withoutPadding().encodeToString(original.getBytes("UTF-8"));
+    System.out.println("After encoding : " + s);
+    byte[]  afterDecoding = Base64.getUrlDecoder().decode(s);
+    System.out.println(new String(afterDecoding, "UTF-8"));
+
+  }
+}
