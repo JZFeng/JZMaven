8df9ca8bd4a2e7aa73c4b8d9195b6575bb5bc914
JZFeng <matthew.fjz@gmail.com>
2019-09-08 09:24:41 -0700

    URL编码

---
+++ JZMaven/src/main/java/com/jz/java/network/security/URLDecode.java
@@ -0,0 +1,15 @@
+package com.jz.java.network.security;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class URLDecode {
+  public static void main(String[] args) throws UnsupportedEncodingException {
+    String original = "URL 参数";
+    String s = URLEncoder.encode(original,"UTF-8");
+    System.out.println(original + " 编码之后：" + s);
+    System.out.println(s + " 解码之后 ：" + URLDecoder.decode(s,"UTF-8"));
+
+  }
+}
