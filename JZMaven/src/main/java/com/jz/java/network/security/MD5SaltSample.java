5f23ba6f5924a1c2f01ab23698c184fb286e0013
JZFeng <matthew.fjz@gmail.com>
2019-09-08 12:33:45 -0700

    MD5是哈希算法（也称摘要算法），可用于验证原始数据是否被篡改和保存用户密码。 加上salt可以防止彩虹攻击。

--- JZMaven/src/main/java/com/jz/java/network/security/MD5SaltSample.java
+++ JZMaven/src/main/java/com/jz/java/network/security/MD5SaltSample.java
@@ -1,18 +1,16 @@
 package com.jz.java.network.security;
 
-
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class MD5Sample {
+public class MD5SaltSample {
   public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-    String original = "MD5摘要算法测试";
+    String password = "helloworld";
+    String salt = "Random Salt";
     MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-    byte[] r = messageDigest.digest(original.getBytes("UTF-8"));
-    System.out.println(String.format("%o",new BigInteger(1, r)));
-    System.out.println(String.format("%d",new BigInteger(1, r)));
-    System.out.println(String.format("%x",new BigInteger(1, r)));
+    byte[] r = messageDigest.digest((password + salt).getBytes("UTF-8"));
+    System.out.println(String.format("%032x", new BigInteger(1, r)));
   }
 }
