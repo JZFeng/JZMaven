d2f1173fbb665a7c763dc400279cd9243f719761
JZFeng <matthew.fjz@gmail.com>
2019-09-08 21:17:25 -0700

    HMac配合SHA1，不是新算法，配合SHA1，MD5等其它哈希算法，相当于带一个随机salt的SHA1

---
+++ JZMaven/src/main/java/com/jz/java/network/security/Hmac.java
@@ -0,0 +1,28 @@
+package com.jz.java.network.security;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Hmac {
+  public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA1");
+    String original = "helloworld";
+    SecretKey secretKey = keyGenerator.generateKey();
+    byte[] r = toHmac("HmacSHA1", secretKey, original.getBytes("UTF-8"));
+    System.out.println(String.format("0" + r.length  * 2 + "x", new BigInteger(1, r)));
+
+  }
+
+  private static byte[] toHmac(String algorithm, SecretKey secretKey, byte[] data) throws NoSuchAlgorithmException, InvalidKeyException {
+    Mac mac = Mac.getInstance(algorithm);
+    mac.init(secretKey);
+    return mac.doFinal(data);
+  }
+}
+
+
