a05534da3e17230920ac3d551893feb617ac7911
JZFeng <matthew.fjz@gmail.com>
2019-09-16 16:19:30 -0700

    PBE demo

---
+++ JZMaven/src/main/java/com/jz/java/network/security/cypher/PBEDemo.java
@@ -0,0 +1,57 @@
+package com.jz.java.network.security.cypher;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+public class PBEDemo {
+  static final String CIPHER_NAME = "PBEwithSHA1and128bitAES-CBC-BC"; // "PBEwithSHA1andDESede";
+
+  // 加密:
+  public static byte[] encrypt(String password, byte[] salt, byte[] input) throws GeneralSecurityException {
+    PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+    SecretKeyFactory skeyFactory = SecretKeyFactory.getInstance(CIPHER_NAME);
+    SecretKey skey = skeyFactory.generateSecret(keySpec);
+    PBEParameterSpec pbeps = new PBEParameterSpec(salt, 1000);
+    Cipher cipher = Cipher.getInstance(CIPHER_NAME);
+    cipher.init(Cipher.ENCRYPT_MODE, skey, pbeps);
+    return cipher.doFinal(input);
+  }
+
+  // 解密:
+  private static byte[] decrypt(String password, byte[] salt, byte[] input) throws GeneralSecurityException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+    SecretKeyFactory skeyFactory = SecretKeyFactory.getInstance(CIPHER_NAME);
+    SecretKey skey = skeyFactory.generateSecret(keySpec);
+    PBEParameterSpec pbeps = new PBEParameterSpec(salt, 1000);
+    Cipher cipher = Cipher.getInstance(CIPHER_NAME);
+    cipher.init(Cipher.DECRYPT_MODE, skey, pbeps);
+    return cipher.doFinal(input);
+  }
+
+  public static void main(String[] args) throws Exception {
+    // 把BouncyCastle作为Provider添加到java.security:
+    Security.addProvider(new BouncyCastleProvider());
+    // 原文:
+    String message = "Hello, world! encrypted using PBE!";
+    // 加密口令:
+    String password = "hello12345";
+    // 16 bytes随机Salt:
+    byte[] salt = SecureRandom.getInstanceStrong().generateSeed(16);
+    System.out.printf("salt: %032x\n", new BigInteger(1, salt));
+    // 加密:
+    byte[] data = message.getBytes(StandardCharsets.UTF_8);
+    byte[] encrypted = encrypt(password, salt, data);
+    System.out.println("encrypted: " + Base64.getEncoder().encodeToString(encrypted));
+    // 解密:
+    byte[] decrypted = decrypt(password, salt, encrypted);
+    System.out.println(new String(decrypted, "UTF-8"));
+  }
+}
