60bb7b0ab29980f0f448c351e1bff1de4034715b
JZFeng <matthew.fjz@gmail.com>
2020-05-31 09:48:38 -0700

    Design pattern_Builder

--- JZMaven/src/main/java/com/jz/java/designpattern/builder/ResourceConfig.java
+++ JZMaven/src/main/java/com/jz/java/designpattern/builder/ResourceConfig.java
@@ -3,71 +3,65 @@ package com.jz.java.designpattern.builder;
 public class ResourceConfig {
 
   private String name;
-
   private int maxTotal;
-
+  private int maxIdle;
   private int minIdle;
 
-  private int maxIdle;
 
   private ResourceConfig(Builder builder) {
     this.name = builder.name;
     this.maxTotal = builder.maxTotal;
-    this.minIdle = builder.minIdle;
     this.maxIdle = builder.maxIdle;
+    this.minIdle = builder.minIdle;
   }
 
-
   public static class Builder {
-    private String name;
 
-    private int maxTotal;
+    private static final String DEFAULT_NAME = "DEFAULT";
+    private static final int DEFAULT_MAX_TOTAL = 8;
+    private static final int DEFAULT_MAX_IDLE = 8;
+    private static final int DEFAULT_MIN_IDLE = 0;
 
-    private int minIdle;
+    private String name = DEFAULT_NAME;
+    private int maxTotal = DEFAULT_MAX_TOTAL ;
+    private int maxIdle = DEFAULT_MAX_IDLE;
+    private int minIdle = DEFAULT_MIN_IDLE;
 
-    private int maxIdle;
-
-
-    public Builder setName(String name) {
+    public Builder name(String name) {
       this.name = name;
       return this;
     }
 
-    Builder setMaxTotal(int maxTotal) {
+    public Builder maxTotal(int maxTotal) {
       this.maxTotal = maxTotal;
       return this;
     }
 
-    Builder setMinIdle(int minIdle) {
-      this.minIdle = minIdle;
-      return this;
-    }
-
-    Builder setMaxIdle(int maxIdle) {
+    public Builder maxIdle(int maxIdle) {
       this.maxIdle = maxIdle;
       return this;
     }
 
-    ResourceConfig build() {
-      if (name == null || name.equals("")) {
-        throw new IllegalArgumentException("name cannot be null");
+    public Builder minIdle(int minIdle) {
+      this.minIdle = minIdle;
+      return this;
+    }
+
+    public ResourceConfig build() throws Exception {
+      if (name == null || name.length() == 0) {
+        throw new Exception("name cannot be empty");
       }
-      if (maxIdle > maxTotal) {
-        throw new IllegalArgumentException("maxIdle cannot be greater than maxTotal");
-      }
-      if (minIdle > maxTotal || minIdle > maxIdle) {
-        throw new IllegalArgumentException("minIdle cannot be greater than maxTotal or maxIdle");
+
+      if (maxTotal <= 0 || minIdle <= 0 || maxIdle <= 0 || maxIdle > maxTotal) {
+        throw new IllegalArgumentException("Parameters are wrong.");
       }
 
       return new ResourceConfig(this);
     }
   }
 
-  public static void main(String[] args) {
-    ResourceConfig resourceConfig = new ResourceConfig.Builder()
-        .setMaxIdle(8)
-        .setMaxTotal(10)
-        .setMinIdle(5)
-        .setName("DB config").build();
+  public static void main(String[] args) throws Exception {
+    ResourceConfig resourceConfig  = new ResourceConfig.Builder().name("abc").maxTotal(10).maxIdle(10).minIdle(0).build();
   }
 }
+
