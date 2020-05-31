233cad2f67c1720a5a5d80e901609e06204193f3
JZFeng <matthew.fjz@gmail.com>
2020-05-30 17:08:31 -0700

    Design pattern_Singleton

--- JZMaven/src/main/java/com/jz/java/designpattern/Singleton/Singleton.java
+++ JZMaven/src/main/java/com/jz/java/designpattern/Singleton/Singleton.java
@@ -1,21 +1,16 @@
 package com.jz.java.designpattern.Singleton;
 
 public class Singleton {
-  //Do not forget volatile keyword;
   private static volatile Singleton instance = null;
 
-  //For testing purpose;
-  private static int num;
-
-  //private constructor;
   private Singleton() {
-    num++;
+
   }
 
   public static Singleton getInstance() {
-    if(instance == null) {
+    if (instance == null) {
       synchronized (Singleton.class) {
-        if(instance == null) {
+        if (instance == null) {
           instance = new Singleton();
         }
       }
@@ -24,8 +19,4 @@ public class Singleton {
     return instance;
   }
 
-  @Override
-  public String toString() {
-    return "Number of instance : " + num;
-  }
 }
