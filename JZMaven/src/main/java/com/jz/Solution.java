30a9c8cddc91362783ba871b1c79cd64557fc969
jzfeng <matthew.fjz@gmail.com>
2021-10-07 09:56:20 -0700

    update

--- JZMaven/src/main/java/com/jz/Solution.java
+++ JZMaven/src/main/java/com/jz/Solution.java
@@ -1,55 +1,96 @@
+/**
+ * @Author jzfeng
+ * @Date 10/2/21-9:27 PM
+ */
+
 package com.jz;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
-public class Solution {
+class Solution {
+    int res = Integer.MAX_VALUE;
 
-  public static void main(String[] args) {
-    Solution solution = new Solution();
-    String[] words = new String[]{"yes","lint","code","yes","code","baby","you","baby","chrome","safari","lint","code","body","lint","code"};
-    solution.topKFrequentWords(words, 3);
-  }
+    public int minimumTimeRequired(int[] jobs, int k) {
+        Arrays.sort(jobs);
+        int[] sum = new int[k];
+        dfs(jobs, sum, jobs.length - 1 );
 
-
-  private String[] topKFrequentWords(String[] words, int k) {
-    // write your code here
-    if (words == null || words.length == 0 || k <= 0) {
-      return words;
-    }
-    if (k > words.length) {
-      k = k % words.length;
+        return res;
     }
 
+    private void dfs(int[] jobs, int[] sum, int pos) {
+        if(pos < 0 ) {
+            int tmp = Arrays.stream(sum).max().getAsInt();
+            res = Math.min(res, tmp);
+            System.out.println(res);
+            return;
+        }
 
-    Map<String, Integer> map = new HashMap<>();
-    for (String word : words) {
-      if (map.containsKey(word)) {
-        map.put(word, map.get(word) + 1);
-      } else {
-        map.put(word, 1);
-      }
+        if( Arrays.stream(sum).max().getAsInt() >= res ) {
+            return;
+        }
+
+        for(int i = 0 ; i < sum.length; i++) {
+            if( i > 0 && sum[i] == sum[i - 1]) {
+                continue;
+            }
+            sum[i] += jobs[pos];
+            dfs(jobs, sum, pos - 1);
+            sum[i] -= jobs[pos];
+        }
     }
 
-    Comparator<Map.Entry<String, Integer>> entryComparator = (e1, e2) -> {
-      int res = e2.getValue() - e1.getValue();
-      if (res == 0) {
-        res = e1.getKey().compareTo(e2.getKey());
-      }
-
-      return res;
-    };
-
-    List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-    Collections.sort(list, entryComparator);
-
-    String[] res = new String[k];
-    int index = 0;
-    for (Map.Entry<String, Integer> entry : list) {
-      res[index++] = entry.getKey();
+    private int[] nextGreaterElement(int[] nums) {
+        int[] res = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while(!stack.isEmpty() && nums[i] >= stack.peek()) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+        return res;
     }
 
-    return res;
-  }
+
+    private String smallestSubsequence(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int[] last = new int[128];
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i)] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if( !visited.add(c)) {
+                continue;
+            }
+            while ( !stack.isEmpty() && c < stack.peek() && i < last[stack.peek()]) {
+                visited.remove(stack.pop());
+            }
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i : stack) {
+            sb.append((char)i);
+        }
+        while(!stack.isEmpty()) {
+            System.out.println( (char)stack.pop().intValue() );
+        }
+
+        return sb.toString();
+    }
 
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "cbacdcbc";
+        String res = solution.smallestSubsequence(s);
+        System.out.println(res);
+    }
 }
