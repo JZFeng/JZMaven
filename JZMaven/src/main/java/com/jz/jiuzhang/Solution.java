e7bba30f97383dbda40fc33fc4bc81857703877d
jzfeng <matthew.fjz@gmail.com>
2022-03-01 12:32:33 -0800

    CyclicBarrier Demo

--- JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
+++ JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
@@ -4,53 +4,53 @@ import java.util.*;
 
 
 public class Solution {
-  public static void main(String[] args) {
-    int[] nums = new int[]{-1};
-    Solution solution = new Solution();
-    double res = solution.findMaxAverage(nums, 1);
-    System.out.println(res);
-  }
+    //无向图。首先判断 edges.length == n - 1;
+    public boolean validTree(int n, int[][] edges) {
+        if (n == 0) {
+            return false;
+        }
 
+        if (edges.length != n - 1) {
+            return false;
+        }
 
-  public int maxScore(int[] cardPoints, int k) {
-    int len = cardPoints.length;
-    int total = Arrays.stream(cardPoints).sum();
-    int sum = 0 ;
-    int min = Integer.MAX_VALUE;
-    for(int i = 0 ; i < len; i++) {
-      sum += cardPoints[i];
-      if( i < k - 1) {
-        continue;
-      }
-      if( i >= k) {
-        sum -= cardPoints[i - k ];
-      }
+        //构建图
+        Map<Integer, Set<Integer>> graph = initializeGraph(n, edges);
 
-      min = Math.max(min, sum );
+        // bfs
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.offer(0);
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            count++;
+            for (Integer neighbor : graph.get(node)) {
+                if (visited.contains(neighbor)) {
+                    continue;
+                }
+                visited.add(neighbor);
+                queue.offer(neighbor);
+            }
+        }
+
+        return (count == n);
     }
 
-    return total - min;
-  }
+    private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
 
-  //滑动窗口的问题
-  public double findMaxAverage(int[] nums, int k) {
-    int len = nums.length;
-    double sum = 0;
-    double res = Double.NEGATIVE_INFINITY;
-    for(int i = 0 ; i < len ; i++) {
-      sum += nums[i];
-      if( i < k - 1 ) {
-        continue;
-      }
-      if( i >= k ) {
-        sum -= nums[i - k ];
-      }
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
-      res = Double.max(res, sum / (double)k );
+        return graph;
     }
-
-    return res;
-  }
-
-
 }
