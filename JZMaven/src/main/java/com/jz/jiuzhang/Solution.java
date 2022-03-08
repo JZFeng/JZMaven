9707861cbe2e6d311f0c2121dd5bdbe4ac7121b1
jzfeng <matthew.fjz@gmail.com>
2022-03-07 23:34:42 -0800

    BlockingQueue

--- JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
+++ JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
@@ -1,75 +1,52 @@
 package com.jz.jiuzhang;
 
+import com.jz.TreeNode;
+
+import javax.swing.text.html.ListView;
 import java.util.*;
 
 
 public class Solution {
-    public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        int min = Integer.MAX_VALUE;
-        int d1 = -1, d2 = -1;
-        for(int i  = 0  ; i < wordsDict.length; i++) {
-            if( word1.equals(wordsDict[i])) {
-                d1 = i;
-            } else if( word2.equals(wordsDict[i]) ) {
-                d2 = i;
-            }
 
-            if( d1 != -1 && d2 != -1 && Math.abs(d1 - d2) < min ) {
-                min = Math.abs(d1 - d2);
-            }
-        }
-        return min;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
     }
 
 
-    //无向图。首先判断 edges.length == n - 1;
-    public boolean validTree(int n, int[][] edges) {
-        if (n == 0) {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+        if(sum % k != 0){
             return false;
         }
 
-        if (edges.length != n - 1) {
-            return false;
+        boolean[] visited = new boolean[nums.length];
+
+        return helper(nums, visited, 0, k, 0, sum / k);
+
+    }
+
+    private boolean helper(int[] nums, boolean[] visited, int start, int remainingGroups, int curSum, int target) {
+        if( remainingGroups == 0 ) {
+            return true;
+        }
+        if(curSum == target) {
+            return helper(nums, visited, 0, remainingGroups - 1, 0, target);
         }
 
-        //构建图
-        Map<Integer, Set<Integer>> graph = initializeGraph(n, edges);
-
-        // bfs
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-
-        queue.offer(0);
-        int count = 1;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            count++;
-            for (Integer neighbor : graph.get(node)) {
-                if (visited.contains(neighbor)) {
-                    continue;
-                }
-                visited.add(neighbor);
-                queue.offer(neighbor);
+        for(int i = start; i < nums.length; i++) {
+            if( visited[i] || curSum + nums[i] > target ) {
+                continue;
             }
+            visited[i] = true;
+            if( helper(nums, visited, i + 1, remainingGroups, curSum + nums[i], target) ) {
+                return true;
+            }
+            visited[i] = false;
         }
 
-        return (count == n);
+        return false;
     }
 
-    private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges) {
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new HashSet<Integer>());
-        }
 
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-
-        return graph;
-    }
 }
