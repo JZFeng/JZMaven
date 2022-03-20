2e8c612064fc52e6f4d409c4f1510fb884e362dc
jzfeng <matthew.fjz@gmail.com>
2022-03-20 11:24:57 -0700

    多隐入一个Game类；

--- JZMaven/src/main/java/com/jz/java/designpattern/statemachine/Entry.java
+++ JZMaven/src/main/java/com/jz/java/designpattern/statemachine/Entry.java
@@ -1,15 +1,16 @@
 /**
  * @Author jzfeng
- * @Date 3/20/22-10:41 AM
+ * @Date 3/20/22-11:14 AM
  */
 
 package com.jz.java.designpattern.statemachine;
 
 public class Entry {
     public static void main(String[] args) throws Exception {
-        MarioStateMachine stateMachine = new MarioStateMachine();
+        Game game = new Game(new MarioStateMachine());
+        game.start();
 
-        System.out.println(stateMachine.getCurrentState().getName() + ":" + stateMachine.getScore());
+        MarioStateMachine stateMachine = game.getStateMachine();
 
         stateMachine.getCurrentState().obtainMushRoom(stateMachine);
         System.out.println(stateMachine.getCurrentState().getName() + ":" + stateMachine.getScore());
@@ -19,6 +20,5 @@ public class Entry {
 
         stateMachine.getCurrentState().meetMonster(stateMachine);
         System.out.println(stateMachine.getCurrentState().getName() + ":" + stateMachine.getScore());
-
     }
 }
