bbc7321f4f7492e6fe5d01a2d00f878acdb2456a
jzfeng <matthew.fjz@gmail.com>
2022-03-28 20:37:37 -0700

    update

--- JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
+++ JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
@@ -6,69 +6,38 @@ import java.util.*;
 
 
 class Solution {
-    //行数=height；列数= Power(2, height) - 1;
-    //每一行的start,end, root肯定是在中间；
 
     public static void main(String[] args) {
-        String url = "https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/";
-        System.out.println(url.length());
-    }
-
-    public TreeNode upsideDownBinaryTree(TreeNode root) {
-        if( root == null || root.left == null) {
-            return root;
-        }
-
-
-        TreeNode newRoot = upsideDownBinaryTree(root.left); //left变成新的根节点；
-        newRoot.left = root.right;
-        newRoot.right = root;
-        root.left = null;
-        root.right = null;
-
-        return newRoot;
+        String s = "abcd";
+        int[] indices = new int[]{0,2};
+        String[] sources = new String[]{"a", "cd"};
+        String[] targets = new String[]{"eee", "ffff"};
+        Solution solution = new Solution();
+        String res = solution.findReplaceString(s, indices, sources, targets);
+        System.out.println(res);
 
     }
-
-    public List<List<String>> printTree(TreeNode root) {
-        List<List<String>> res = new ArrayList<>();
-        int height = height(root);
-        int rows = height, cols = (int) (Math.pow(2, height) - 1);
-        //初始化
-        List<String> row = new ArrayList<>();
-        for(int i = 0 ; i < cols; i++) {
-            row.add("");
-        }
-        for(int i = 0 ; i < rows; i++) {
-            res.add(new ArrayList<>(row));
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        int len = s.length();
+        int[] matches = new int[len];
+        Arrays.fill(matches, -1);
+        for (int i = 0; i < indices.length; i++) {
+            if (s.substring(indices[i], indices[i] + sources[i].length()).equals(sources[i])) {
+                matches[indices[i]] = i;
+            }
         }
 
-        //height,也是numOfRow；
-        helper(root, res, 0, cols -1, 0);
-
-        return res;
-
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; ) {
+            if(matches[i] != -1) {
+                sb.append(targets[matches[i]]);
+                i += sources[matches[i]].length();
+            } else {
+                sb.append(s.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
     }
 
-    private void helper( TreeNode root, List<List<String>> res, int start, int end , int curRow ) {
-        if(root == null ) {
-            return;
-        }
-
-        int mid = start + (end - start) / 2;
-        res.get(curRow).set(mid, root.val + "");
-        helper( root.left, res, start, mid - 1, curRow + 1 );
-        helper(root.right, res, mid + 1, end, curRow + 1);
-
-    }
-
-
-    private int height(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-
-        return Math.max( height(root.left) , height(root.right )) + 1;
-    }
 }
