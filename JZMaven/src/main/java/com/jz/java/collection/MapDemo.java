606c973b103323829f4b74c73e58e060e0df1d7d
jz <matthew.fjz@gmail.com>
2022-04-23 22:52:59 -0700

    union find

--- JZMaven/src/main/java/com/jz/java/collection/MapDemo.java
+++ JZMaven/src/main/java/com/jz/java/collection/MapDemo.java
@@ -15,6 +15,21 @@ public class MapDemo {
 
         System.out.println(map.get(new Person("Zhu", 20)));
 
+        Map<String, Integer> prices = new HashMap<>();
+        // 往HashMap中添加映射项
+        prices.put("Shoes", 200);
+        prices.put("Bag", 300);
+        prices.put("Pant", 150);
+        System.out.println("HashMap: " + prices);
+
+        // 计算 Shirt 的值
+        int shirtPrice = prices.computeIfAbsent("Shirt", key -> 280);
+        System.out.println("Price of Shirt: " + shirtPrice);
+
+        // 输出更新后的HashMap
+        System.out.println("Updated HashMap: " + prices);
+
     }
+
 
 }
