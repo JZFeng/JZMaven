dcf516a9d0df0840b2449ec26416e0cbb7b1a258
jzfeng <matthew.fjz@gmail.com>
2022-04-24 21:02:10 -0700

    update

--- JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
+++ JZMaven/src/main/java/com/jz/jiuzhang/Solution.java
@@ -3,50 +3,119 @@ package com.jz.jiuzhang;
 
 import java.util.*;
 
-class Solution {
+public class Solution {
+    //无向图。首先判断 edges.length == n - 1;
+    //此题可以转换为无向图找环的问题；
+    public boolean validTree2(int n, int[][] edges) {
+        DSU dsu = new DSU(n);
+        for(int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            if( dsu.find(a) == dsu.find(b) ) {
+                return false;
+            }
+            dsu.union(a, b);
+        }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int costs = solution.minCostToSupplyWater(3, new int[]{1, 2, 2}, new int[][]{{1, 2, 1}, {2, 3, 1}});
-        System.out.println(costs);
+        return edges.length == n - 1;
     }
 
-    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        Map<Integer, Map<Integer,Integer>> graph = new HashMap<>();
-        // 图，起点 -> Map（终点列表，cost）；
-        //加虚拟节点；
-        for(int i = 1; i <= n ; i++) {
-            graph.computeIfAbsent(0, k -> new HashMap<>() ).put(i, wells[i - 1]);
-        }
-        //建图；
-        for(int i = 0 ; i < pipes.length; i++) {
-            int[] edge = pipes[i];
-            int x = edge[0], y = edge[1],cost = edge[2];
+    class DSU {
+        int[] parent;
 
-            int minFrom0To1 = graph.computeIfAbsent(x, k -> new HashMap<>()).getOrDefault(y, Integer.MAX_VALUE);
-            int minFrom1To0 = graph.computeIfAbsent(y, k -> new HashMap<>()).getOrDefault(x, Integer.MAX_VALUE);
-            graph.get(x).put(y, Math.min(cost, minFrom0To1));
-            graph.get(y).put(x, Math.min(cost, minFrom1To0));
+        DSU(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
         }
 
-        int costs = 0 ;
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> a[1] - b[1]);
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            parent[find(x)] = find(y);
+        }
+    }
+
+    public boolean validTree1(int n, int[][] edges) {
+        if (n == 0) {
+            return false;
+        }
+
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        //构建图
+        Map<Integer, List<Integer>> graph = buildGraph(n, edges);
+
+        // bfs
+        Queue<Integer> queue = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
-        pq.offer(new int[]{0,0}); // PQ里存放的是{curNode, cost};
-        while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNode = cur[0], cost = cur[1];
-            if( !visited.add(curNode) ) {
-                continue;
-            }
-            costs += cost;
-            for(int neighbor : graph.getOrDefault(curNode, new HashMap<>()).keySet()) {
-                if(!visited.contains(neighbor)) {
-                    pq.offer(new int[]{neighbor, graph.get(curNode).get(neighbor)});
+        queue.offer(0);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            visited.add(node);
+            for (Integer neighbor : graph.get(node)) {
+                if (visited.contains(neighbor)) {
+                    continue;
                 }
+                queue.offer(neighbor);
             }
         }
 
-        return costs;
+        return (visited.size() == n);
+    }
+
+    public boolean validTree(int n , int[][] edges) {
+        if (n == 0) {
+            return false;
+        }
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        Map<Integer, List<Integer>> graph = buildGraph(n , edges);
+        boolean[] visited = new boolean[n];
+        if( hasCycle(graph, 0, visited, -1) ) {
+            return false;
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            if(!visited[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(Map<Integer, List<Integer>> graph, int cur, boolean[] visited, int parent) {
+        visited[cur] = true;
+        for(int i = 0 ; i < graph.get(cur).size(); i++) {
+            if(!visited[i])
+        }
+    }
+
+
+    private Map<Integer, List<Integer>> buildGraph(int n, int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<Integer>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        return graph;
     }
 }
