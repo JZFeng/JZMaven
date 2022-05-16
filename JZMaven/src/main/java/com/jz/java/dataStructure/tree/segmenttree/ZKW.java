23d03c6f153aa7c94cdd81826826f69ef92874dd
JZ <jzfeng@ebay.com>
2022-05-16 16:56:31 -0700

    segment tree 传统实现，top down

--- JZMaven/src/main/java/com/jz/java/dataStructure/tree/segmenttree/ZKW.java
+++ JZMaven/src/main/java/com/jz/java/dataStructure/tree/segmenttree/ZKW.java
@@ -2,6 +2,12 @@ package com.jz.java.dataStructure.tree.segmenttree;
 
 public class ZKW {
 
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,6,7,3};
+        ZKW zkw = new ZKW(nums);
+        zkw.sumRange(1,3);
+    }
+
     int n;
     int[] st;
 
@@ -24,12 +30,13 @@ public class ZKW {
             st[i] += diff;
         }
     }
-    //区间查询；
-    public int rangeSum(int i, int j ) {
+    //区间查询；左偶 右奇
+    public int sumRange(int i, int j ) {
         int res = 0 ;
         for( i = i + n, j = j + n; i <= j; i /= 2, j /= 2 ) {
+            //left boundary 为右孩子；right boundary为左孩子；
             if( i % 2 == 1) res += st[i++];
-            if(j % 2 == 0) res += st[j--];
+            if( j % 2 == 0) res += st[j--];
         }
         return res;
     }
