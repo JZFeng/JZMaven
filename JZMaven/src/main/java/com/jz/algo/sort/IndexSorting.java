35553cc49194ab2765fb2bb81ad82336cb1cfadf
JZFeng <matthew.fjz@gmail.com>
2023-03-26 15:29:02 -0700

    updte index sorting,有很多坑哪。

--- JZMaven/src/main/java/com/jz/algo/sort/IndexSorting.java
+++ JZMaven/src/main/java/com/jz/algo/sort/IndexSorting.java
@@ -9,23 +9,22 @@ public class IndexSorting {
     //[268. Missing Number](https://leetcode.com/problems/missing-number/)
     public int missingNumber(int[] nums) {
         int len = nums.length;
-
-        for(int i = 0 ; i < len; i++) {
-            while( nums[i] != i && nums[i] >= 0 && nums[i] < len && nums[nums[i]] != nums[i] ) {
-                swap(nums, i, nums[i]);
+        for (int i = 0; i < len; i++) {
+            while (i < len && nums[i] != i && nums[i] < len && nums[nums[i]] != nums[i] ) {
+                swap(nums, i ,nums[i]);
+                // int tmp = nums[nums[i]]; //这里也有坑，必须保存nums[nums[i]],最好的方法就是独立写一个swap函数；
+                // nums[nums[i]] = nums[i];
+                // nums[i] = tmp;
             }
         }
-
-        for(int i = 0 ; i < len; i++) {
-            if( nums[i] != i ) {
-                return i;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                return i; //这也是个坑；
             }
         }
-        //这里是个坑，要return nums.length;
-        return nums.length;
+        return nums.length;  //这里是个坑，要return nums.length;
     }
-
-    private void swap(int[] nums, int i, int j ) {
+    private void swap(int[] nums, int i, int j){
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
