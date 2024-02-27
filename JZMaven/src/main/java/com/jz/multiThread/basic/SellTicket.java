6f1637ade05e8a870ba493e12315758f860d02b0
jzfeng <matthew.fjz@gmail.com>
2024-02-27 09:26:05 -0800

    update

--- JZMaven/src/main/java/com/jz/multiThread/basic/SellTicket.java
+++ JZMaven/src/main/java/com/jz/multiThread/basic/SellTicket.java
@@ -1,13 +1,15 @@
 package com.jz.multiThread.basic;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SellTicket {
 
     public static void main(String[] args) throws InterruptedException {
-        Ticket ticket = new Ticket();
-        Thread window1 = new Thread(ticket, "Window1");
-        Thread window2 = new Thread(ticket, "Window2");
-        Thread window3 = new Thread(ticket, "Window3");
-        Thread window4 = new Thread(ticket, "Window4");
+        SellingTicket sellingTicket = new SellingTicket();
+        Thread window1 = new Thread(sellingTicket, "Window1");
+        Thread window2 = new Thread(sellingTicket, "Window2");
+        Thread window3 = new Thread(sellingTicket, "Window3");
+        Thread window4 = new Thread(sellingTicket, "Window4");
         window1.start();
         window2.start();
         window3.start();
@@ -21,32 +23,30 @@ public class SellTicket {
 
 }
 
-class Ticket implements Runnable {
+class SellingTicket implements Runnable {
     private int total_tickets = 400;
 
-    private final Object lock = new Object();
+    private final ReentrantLock reentrantLock = new ReentrantLock();
 
     @Override
     public void run() {
-
-        synchronized (lock) {
+        reentrantLock.lock();
+        try {
             while (true) {
                 if (total_tickets <= 0) {
                     System.out.println("Sold Out!");
                     break;
                 }
-
+                //Simulating selling a ticket;
+                Thread.sleep(1000);
                 System.out.println(
                         Thread.currentThread().getName() + " sold a ticket; Remaining: " +
                                 total_tickets--);
             }
-        }
-
-        try {
-            //Simulating selling a ticket;
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        reentrantLock.unlock();
     }
 }
